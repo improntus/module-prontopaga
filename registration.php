1c136942dac0df9197ec6c32d178efa12f1ad914
mauromm <mauromaximilianomartinez@hotmail.com>
2025-07-16 19:55:07 -0300

    Compatibility with Adobe Commerce and Magento Open Source 2.4.8-p1

--- registration.php
+++ registration.php
@@ -1,3 +1,4 @@
-<?phpuse Magento\Framework\Component\ComponentRegistrar;
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Improntus_ProntoPaga', __DIR__);
